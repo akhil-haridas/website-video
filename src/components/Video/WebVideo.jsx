d3bfae32fcc8319fc89bb3376cd5c2f748953adf
akhil-haridas <4khilharidas@gmail.com>
2024-08-28 14:09:52 +0530

    components for the video and html

---
+++ src/components/Video/WebVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WebVideo = () => {
+  return (
+    <div>WebVideo</div>
+  )
+}
+
+export default WebVideo
---
+++ src/components/Website/WebHtml.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WebHtml = () => {
+  return (
+    <div>WebHtml</div>
+  )
+}
+
+export default WebHtml
